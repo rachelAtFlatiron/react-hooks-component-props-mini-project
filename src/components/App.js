23be33176fd7cfe9bc82bba928b6ce13ced4b15c
Rachel Katz <rachel.katz@flatironschool.com>
2022-08-09 09:40:45 -0400

    starting fresh

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,14 @@
 import React from "react";
 import blogData from "../data/blog";
-import Header from './Header';
-import About from './About';
-import ArticleList from './ArticleList';
 
+//Can you pass props directly up from a parent?
+//What does dynamically mean
 console.log(blogData);
 
 function App() {
-  return (
-    <div className="App">
-      <Header name={blogData.name} />
-      <About src={blogData.image} about={blogData.about} />
-      <ArticleList posts={blogData.posts} />
-    </div>
-  );
+    return(
+        <div></div>
+    )
 }
 
 export default App;
